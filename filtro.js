f40ab11648a0e6052ddeb0d78c8e9e04ac77a925
mariaabi82 <maria.abi1982@gmail.com>
2020-02-07 20:36:30 -0300

    bajo archivos y creo repo

---
+++ filtro.js
@@ -0,0 +1,36 @@
+// A partir de la siguiente lista de productos, 
+// hacer una web siguiendo el maquetado de ejemplo. 
+
+// Al escribir en el input el color o el tipo de un producto, 
+// se deben filtrar los productos. 
+// Funciona tanto apretando enter como haciendo clic en el boton. 
+
+
+
+const productos = [
+  {
+    nombre: 'Zapato negro',
+    tipo: 'zapato',
+    color: 'negro',
+    img: './img/taco-negro.jpg',
+  },
+  {
+    nombre: 'Zapato azul',
+    tipo: 'zapato',
+    color: 'azul',
+    img: './img/taco-azul.jpg',
+  },
+  {
+    nombre: 'Bota negra',
+    tipo: 'bota',
+    color: 'negro',
+    img: './img/bota-negra.jpg',
+  },
+  { nombre: 'Bota azul', tipo: 'bota', color: 'azul', img: './img/bota-azul.jpg' },
+  {
+    nombre: 'Zapato rojo',
+    tipo: 'zapato',
+    color: 'rojo',
+    img: './img/zapato-rojo.jpg',
+  },
+];
